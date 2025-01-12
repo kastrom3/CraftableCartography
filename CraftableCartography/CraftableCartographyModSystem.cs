e160575d1299783b3bb55f54e9cea868efe1d72f
Professor Cupcake <profcupcake@hotmail.co.uk>
2025-01-12 20:56:17 +0000

    Map no longer tracks player, and attempts to save/load last view

--- CraftableCartography/CraftableCartographyModSystem.cs
+++ CraftableCartography/CraftableCartographyModSystem.cs
@@ -1,11 +1,14 @@
 ﻿using CraftableCartography.MapLayers;
 using HarmonyLib;
+using System;
 using System.Linq;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
+using Vintagestory.API.Datastructures;
 using Vintagestory.API.MathTools;
 using Vintagestory.API.Server;
 using Vintagestory.GameContent;
+using static CraftableCartography.Lib.CCConstants;
 
 namespace CraftableCartography
 {
@@ -60,6 +63,7 @@ namespace CraftableCartography
             harmony.UnpatchAll(patchName);
         }
 
+        
         [HarmonyPrefix]
         [HarmonyPatch(typeof(WorldMapManager), nameof(WorldMapManager.RegisterDefaultMapLayers))]
         public static bool RegisterDefaultMapLayers(WorldMapManager __instance)
@@ -77,9 +81,63 @@ namespace CraftableCartography
         {
             GuiElementMap elemMap = __instance.SingleComposer.GetElement("mapElem") as GuiElementMap;
 
-            ICoreClientAPI capi = Traverse.Create(__instance).Field("capi").GetValue<ICoreClientAPI>();
+            Traverse traverse = Traverse.Create(__instance);
 
-            elemMap.CenterMapTo(capi.World.DefaultSpawnPosition.AsBlockPos);
+            ICoreClientAPI capi = traverse.Field("capi").GetValue<ICoreClientAPI>();
+
+            BlockPos pos = capi.World.Player.Entity.Attributes.GetBlockPos(MapOpenCoordsAttr, capi.World.DefaultSpawnPosition.AsBlockPos);
+
+            elemMap.CenterMapTo(pos);
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(GuiDialogWorldMap), nameof(GuiDialogWorldMap.TryClose))]
+        public static void StoreMapLocation(GuiDialogWorldMap __instance)
+        {
+            ICoreClientAPI capi = Traverse.Create(__instance).Field("capi").GetValue<ICoreClientAPI>();
+            GuiElementMap elemMap = __instance.SingleComposer.GetElement("mapElem") as GuiElementMap;
+            Cuboidd curBlockViewBounds = elemMap.CurrentBlockViewBounds;
+            capi.ShowChatMessage("curBlockViewBounds:-\n" +
+                "X1: " + curBlockViewBounds.X1 + " X2: " + curBlockViewBounds.X2 + "\n" +
+                "Y1: " + curBlockViewBounds.Y1 + " Y2: " + curBlockViewBounds.Y2 + "\n" +
+                "Z1: " + curBlockViewBounds.Z1 + " Z2: " + curBlockViewBounds.Z2);
+            BlockPos pos = new(
+                (int)(curBlockViewBounds.X1 + curBlockViewBounds.X2) / 2,
+                (int)(curBlockViewBounds.Y1 + curBlockViewBounds.Y2) / 2,
+                (int)(curBlockViewBounds.Z1 + curBlockViewBounds.Z2) / 2
+                );
+            capi.ShowChatMessage("Map closed, centre was about " + pos.ToString() + " (" + pos.SubCopy(capi.World.DefaultSpawnPosition.AsBlockPos).ToString() + ")");
+
+            capi.World.Player.Entity.Attributes.SetBlockPos(MapOpenCoordsAttr, pos);
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(GuiElementMap), nameof(GuiElementMap.PostRenderInteractiveElements))]
+        public static void StopMapTrackingPlayer(GuiElementMap __instance)
+        {
+            if (__instance != null)
+            {
+                Traverse traverse = Traverse.Create(__instance);
+
+                if (traverse != null)
+                {
+                    ICoreClientAPI capi = __instance.Api;
+                    if (capi != null)
+                    {
+                        if (capi.World != null)
+                        {
+                            if (capi.World.Player != null)
+                            {
+                                if (capi.World.Player.Entity != null)
+                                {
+                                    Vec3d playerPos = capi.World.Player.Entity.Pos.XYZ;
+                                    traverse.Field("prevPlayerPos").GetValue<Vec3d>().Set(playerPos.X, playerPos.Y, playerPos.Z);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- CraftableCartography/Lib/CCConstants.cs
+++ CraftableCartography/Lib/CCConstants.cs
@@ -3,5 +3,6 @@
     public static class CCConstants
     {
         public const string ShowOnMapAttr = "CC_showOnMap";
+        public const string MapOpenCoordsAttr = "CC_mapOpenCoords";
     }
 }
