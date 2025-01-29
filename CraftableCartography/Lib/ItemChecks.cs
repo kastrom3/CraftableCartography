bf1a5fd163e06b0ae581b148f73f6d66c4737f75
Professor Cupcake <profcupcake@hotmail.co.uk>
2025-01-29 18:00:21 +0000

    fixed item check for variants

--- CraftableCartography/Lib/ItemChecks.cs
+++ CraftableCartography/Lib/ItemChecks.cs
@@ -5,49 +5,50 @@ namespace CraftableCartography.Lib
 {
     public static class ItemChecks
     {
-        public const string jpsCode = "craftablecartography:jps";
-        public const string compassCode = "craftablecartography:compasstemporal";
-        public const string temporalSextantCode = "craftablecartography:sextanttemporal";
-        public const string mapCode = "craftablecartography:map";
+        public const string jpsCode = "jps";
+        public const string compassCode = "compasstemporal";
+        public const string temporalSextantCode = "sextanttemporal";
+        public const string mapCode = "map";
+        public const string modDomain = "craftablecartography";
 
-        public static bool GenericItemCheck(ICoreClientAPI capi, string itemCode)
+        public static bool GenericItemCheck(ICoreClientAPI capi, string itemCode, string domain)
         {
             foreach (ItemSlot slot in capi.World.Player.InventoryManager.GetHotbarInventory())
                 if (slot.Itemstack != null)
                     if (slot.Itemstack.Item != null)
-                        if (slot.Itemstack.Item.Code == itemCode)
+                        if (slot.Itemstack.Item.Code.FirstCodePart() == itemCode && slot.Itemstack.Item.Code.Domain == domain)
                             return true;
             return false;
         }
-        public static bool GenericItemsCheck(ICoreClientAPI capi, string[] itemCodes)
+        public static bool GenericItemsCheck(ICoreClientAPI capi, string[] itemCodes, string domain)
         {
             foreach (ItemSlot slot in capi.World.Player.InventoryManager.GetHotbarInventory())
                 if (slot.Itemstack != null)
                     if (slot.Itemstack.Item != null)
                         if (slot.Itemstack.Item.Code != null)
                             foreach (string itemCode in itemCodes)
-                                if (slot.Itemstack.Item.Code == itemCode)
+                                if (slot.Itemstack.Item.Code.FirstCodePart() == itemCode && slot.Itemstack.Item.Code.Domain == domain)
                                     return true;
 
             return false;
         }
         public static bool HasTemporalSextant(ICoreClientAPI capi)
         {
-            return GenericItemCheck(capi, temporalSextantCode);
+            return GenericItemCheck(capi, temporalSextantCode, modDomain);
         }
 
         public static bool HasTemporalCompass(ICoreClientAPI capi)
         {
-            return GenericItemCheck(capi, compassCode);
+            return GenericItemCheck(capi, compassCode, modDomain);
         }
 
         public static bool HasMap(ICoreClientAPI capi)
         {
-            return GenericItemCheck(capi, mapCode);
+            return GenericItemCheck(capi, mapCode, modDomain);
         }
         public static bool HasJPS(ICoreClientAPI capi)
         {
-            return GenericItemCheck(capi, jpsCode);
+            return GenericItemCheck(capi, jpsCode, modDomain);
         }
     }
 }
